acb213baf273cbcf01d39982a801b21fa73d66e7
rowan <roulton@slack-corp.com>
2023-02-01 09:56:52 +1300

    Add internal ts package

--- packages/foo/foo.ts
+++ packages/foo/foo.ts
@@ -1,8 +1,12 @@
 import builtThing from "@slack/built-thing";
 import rawThing from "@slack/raw-thing";
 import { print } from "../../src/util";
+import { thing } from "@slack/typed-thing";
+import type { Baz } from "@slack/typed-thing";
 
 function foo() {
+  const b: Baz = thing;
+  console.log(b.id, b.name);
   print("foo");
   builtThing();
   rawThing();
